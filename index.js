3a38f4fca0edf7d7672f2e3761323c43980a82b5
ablaesther <ablaesther23@gmail.com>
2025-02-27 13:33:34 +0000

    add new item

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function showService(service) {
-  alert(`You clicked on ${service}`);
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll('.filter-btn');
+  const imageContainer = document.querySelector('.image-container');
+
+  filterButtons.forEach(button => {
+      button.addEventListener('click', filterImages);
+  });
+
+  function filterImages(e) {
+      const category = e.target.getAttribute('data-filter');
+      const images = document.querySelectorAll('.image');
+
+      images.forEach(image => {
+          image.style.display = 'none';
+
+          if (category === 'all' || image.classList.contains(category)) {
+              image.style.display = 'block';
+          }
+      });
+  }
+});
